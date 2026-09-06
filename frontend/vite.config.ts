e5672a6a072f91242d6f9cc5e0d35d8476560b04
Maulik Joshi <jmaulik21@gmail.com>
2026-09-06 21:13:59 +0545

    hi

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,11 @@ export default defineConfig({
     outDir: '../public/seema',
     emptyOutDir: true,
   },
+  css: {
+    postcss: {
+      plugins: [],
+    },
+  },
   plugins: [react()],
   server: {
     port: 5173,
